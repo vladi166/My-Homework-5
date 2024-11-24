b1700260f49f9e2c4c0fef2024e72fd018ad82c0
Vladislav Andreev <vladireft96@mail.ru>
2024-11-24 19:35:44 +0500

    Параметризированные тесты

--- src/test/java/ru/netology/javaqa/MyHomework5/services/IncomeExpensesServiceTest.java
+++ src/test/java/ru/netology/javaqa/MyHomework5/services/IncomeExpensesServiceTest.java
@@ -1,42 +1,19 @@
 package ru.netology.javaqa.MyHomework5.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class IncomeExpensesServiceTest {
 
-@Test
-    void NumberOfMonthsOfRestFirstTest () {
-    IncomeExpensesService service = new IncomeExpensesService();
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/IncomeExpenses.csv")
+    void NumberOfMonthsOfRestFirstTest(int expected, int income, int expenses, int treshold) {
+        IncomeExpensesService service = new IncomeExpensesService();
 
-    int income = 10000;
-    int expenses = 3000;
-    int treshhold = 20000;
-    int expected = 3;
+        int actual = service.calculate(income, expenses, treshold);
 
-    int actual = service.calculate(income, expenses, treshhold);
-
-    Assertions.assertEquals(expected, actual);
-}
-
-@Test
-    void NumberOfMonthsOfRestSecondTest () {
-    IncomeExpensesService service = new IncomeExpensesService();
-
-    int income = 100000;
-    int expenses = 60000;
-    int treshhold = 150000;
-    int expected = 2;
-
-    int actual = service.calculate(income, expenses, treshhold);
-
-    Assertions.assertEquals(expected, actual);
-}
-
-
-
-
-
-
+        Assertions.assertEquals(expected, actual);
+    }
 }
 
